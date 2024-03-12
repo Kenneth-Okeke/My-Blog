a9bbbff2e2037fced17e8328b1418266a23c58d8
Ken_2k24 <kennethokeke56@gmail.com>
2024-03-12 22:47:22 +0000

    Initial addition of code for the layout of the blog

---
+++ Mytechblog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Dummy data for posts
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2024'
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = posts[post_id]
+    return render_template('post.html', post=post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,3 @@
+# My Tech Blog
+This is a personal blog project to share my thoughts and learnings about technology.
+
